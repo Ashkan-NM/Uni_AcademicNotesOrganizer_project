1db3bef43faa411bceebdfd0ecfee787c4159983
AshkanNM <ashkannamayanmarghub@gmail.com>
2026-07-25 22:22:46 +0330

    Add forms for Course and Note

---
+++ notes/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Course, Note
+
+class CourseForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ['title', 'description']
+
+class NoteForm(forms.ModelForm):
+    class Meta:
+        model = Note
+        fields = ['title', 'description', 'content']
